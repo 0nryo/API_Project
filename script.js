cfa40b67c143f1dc6ee79831ad087ff5885b7294
0nryo <120467446+0nryo@users.noreply.github.com>
2023-05-07 18:11:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+//Erste API: Bored API, Aktivitäten getter
+const btnActivity = document.getElementById("btnActivity");
+const lastActivities = document.getElementById("lastActivities");
+let previousActivity = null;
+
+btnActivity.addEventListener("click", () => {
+  fetch("https://www.boredapi.com/api/activity")
+    .then(response => response.json())
+    .then(data => {
+      //Erstellt ein neues p element für die neuste Aktivität
+      const newActivity = document.createElement("p");
+      newActivity.textContent = data.activity;
+      newActivity.classList.add("output");
+
+      //Wenn es eine vorherige aktivität gibt, wird diese angehängt
+      if (previousActivity) {
+        lastActivities.insertBefore(previousActivity, lastActivities.firstChild);
+      }
+
+      //Updated den aktivitäten output mit der neusten aktivität
+      if (data.activity) {
+        document.getElementById("activity").textContent = data.activity;
+      }
+
+      //Speichert die neuste aktivität mit einer vorherigen
+      previousActivity = newActivity;
+
+      
+      //Löscht die äterste aktivität, wenn es mehr als zwei alte gibt
+      if (lastActivities.children.length > 2) {
+        lastActivities.removeChild(lastActivities.children[2]);
+      }
+    })
+    .catch(error => {
+      document.getElementById("activity").textContent = "Oops, something went wrong!";
+      console.error(error);
+    });
+});
+
+//Age API
+
+function predictAge() {
+  const name = document.getElementById("name").value;
+  fetch(`https://api.agify.io?name=${name}&country_id=AT`)
+    .then(response => response.json())
+    .then(data => {
+      const age = data.age;
+      document.getElementById("age").textContent = age;
+    })
+    .catch(error => {
+      console.error(error);
+      alert("An error occurred while predicting your age. Please try again later.");
+    });
+}
+
+function predictNationality() {
+  const name = document.getElementById("name").value;
+  fetch(`https://api.nationalize.io/?name=${name}`)
+    .then(response => response.json())
+    .then(data => {
+      const country = data.country[0].country_id;
+      document.getElementById("nationality").textContent = country;
+    })
+    .catch(error => {
+      console.error(error);
+      alert("An error occurred while predicting your nationality. Please try again later.");
+    });
+  }
+//Dog API
+
+
+function getRandomDogImage() {
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then(response => response.json())
+    .then(data => {
+      const dogImage = data.message;
+      document.getElementById("dog").setAttribute("src", dogImage);
+    })
+    .catch(error => {
+      console.error(error);
+      alert("An error occurred while fetching the dog image. Please try again later.");
+    });
+}
